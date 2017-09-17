169b9dffef232d9b2b976bf51bb10767d50ad3b5
Patricia Pampanelli <patricia.pampanelli@gmail.com>
2017-09-17 23:00:04 +0000

    Second model

--- trainning.py
+++ trainning.py
@@ -2,6 +2,15 @@ import csv
 import cv2
 import numpy as np
 
+def read_image(source_path):
+	#print(source_path)
+	filename = source_path.split('/')[-1]
+	#print(filename)
+	filename = './data/IMG/'+ filename.split('\\')[-1]
+	#print(filename)
+	image = cv2.imread(filename)
+	return image
+
 lines = []
 with open('./data/driving_log.csv') as csvfile:
 	reader = csv.reader(csvfile)
@@ -11,29 +20,55 @@ with open('./data/driving_log.csv') as csvfile:
 images = []
 measurements = []
 for line in lines:
-	source_path = line[0]
-	if source_path == 'center':
-		continue
-	filename = source_path.split('/')[-1]
-	filename = 'data/IMG/'+ filename.split('\\')[-1]
+	steering_center = float(line[3])
+	# create adjusted steering measurements for the side camera images
+	correction = 0.2 # this is a parameter to tune
+	steering_left = steering_center + correction
+	steering_right = steering_center - correction
+   
+	# Center
+	images.append(read_image(line[0]))
+	measurements.append(steering_center)
+	# Left
+	images.append(read_image(line[1]))
+	measurements.append(steering_left)
+	# Right
+	images.append(read_image(line[2]))
+	measurements.append(steering_right)
 
-	current_path = './data/IMG' + filename
-	image = cv2.imread(current_path)
-	images.append(image)
-	measurement = float(line[3])
-	measurements.append(measurement)
+augmented_images, augmented_measurements = [], []
+for image, measurement in zip(images, measurements):
+	augmented_images.append(image)
+	augmented_measurements.append(measurement)
+	augmented_images.append(cv2.flip(image, flipCode=1))
+	augmented_measurements.append(measurement*-1.0)
 
-X_train = np.array(images)
-y_train = np.array(measurements)
+X_train = np.array(augmented_images)
+y_train = np.array(augmented_measurements)
 
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
+from keras.layers.convolutional import Cropping2D
 
 model  = Sequential()
+# Normalize and mean centering the data 
 model.add(Lambda(lambda x: x/255.0 - 0.5, input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((70,25), (0,0))))
+model.add(Convolution2D(24, 5, 5, subsample=(2,2), activation = 'relu'))
+model.add(Convolution2D(36, 5, 5, subsample=(2,2), activation = 'relu'))
+model.add(Convolution2D(48, 5, 5, subsample=(2,2), activation = 'relu'))
+model.add(Convolution2D(64, 3, 3, activation = 'relu'))
+model.add(Convolution2D(64, 3, 3, activation = 'relu'))
 model.add(Flatten())
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
 model.add(Dense(1))
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=3)
+
+
 
 model.save('model.h5')
